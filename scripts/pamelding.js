095cd222d66c4a508f509d9b27eba1698169a3cb
Dev Hansen <58080810+DevEskild@users.noreply.github.com>
2022-02-11 17:02:39 +0100

    Add files via upload

--- scripts/pamelding.js
+++ scripts/pamelding.js
@@ -51,10 +51,6 @@ function leggTil(navn, etternavn, alder) {
             alder: alder
         })
     }
-    else {
-        alert("Vennligs velg et arragement du ønsker å melde deg på!")
-        throw new Error("Ingen arragement")
-    }
     listeUt();
 }
 
